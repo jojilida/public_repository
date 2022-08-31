8f7d08ce9dd6d83e450425d61e9707ac2c512518
jojilida <87890302+jojilida@users.noreply.github.com>
2022-08-31 12:08:32 +0800

    Update ManageController.cs

--- LIB/LIB/Controllers/ManageController.cs
+++ LIB/LIB/Controllers/ManageController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Oracle.ManagedDataAccess.Client;
 using System.Data;
@@ -31,30 +31,39 @@ namespace LIB.Controllers
             JsonString = JsonConvert.SerializeObject(datatable.Tables[0]);
             return JsonString;
         }
-        [HttpGet]
+        [HttpPost]
         public string GetLoanBooks(String userid)
         {
             string result = "";
             DataSet datatable = new DataSet();
-            datatable = DbHelperOra.Query("select * from MY_LOAN_BOOKS where LOAN_PEOPLE_ID=" + userid);
+            datatable = DbHelperOra.Query("select * from MY_LOAN_BOOKS natural join MY_BOOKINFO where LOAN_PEOPLE_ID=" + userid);
 
             string JsonString = string.Empty;
             JsonString = JsonConvert.SerializeObject(datatable.Tables[0]);
             return JsonString;
         }
-        [HttpGet]
-        public bool ChangePassword(String userid ,String password)
+        [HttpPost]
+        public string GetDamagedBooks(String userid)
         {
-            string sqlstr = "select * from MY_USER where and USER_ID=" + userid;
-            var data = DbHelperOra.Query("select * from MY_USER");
-            var judge1 = DbHelperOra.Exists(sqlstr);
-            if (!judge1) return false;
-            var strchange = "update MY_USER set PASSWORD=:password where USER_ID=:userid";
-            List<OracleParameter> oracleParameters = new List<OracleParameter>();
-            oracleParameters.Add(new OracleParameter(":password", password));
-            oracleParameters.Add(new OracleParameter(":userid", userid));
-            DbHelperOra.ExecuteSql(strchange, oracleParameters.ToArray());
-            return true;
+            string result = "";
+            var datatable = DbHelperOra.Query("select * from MY_DAMAGED_BOOKS natural join MY_BOOKINFO where USER_ID='" + userid + "' and DAMAGE_STATE=1");
+            string JsonString = string.Empty;
+            JsonString = JsonConvert.SerializeObject(datatable.Tables[0]);
+            return JsonString;
+        }
+        [HttpPost]
+        public int GetLoanBooksNum(String userid)
+        {
+            var data = DbHelperOra.Query("select * from MY_LOAN_BOOKS where LOAN_PEOPLE_ID=" + userid);
+            int num = data.Tables[0].Rows.Count;
+            return num;
+        }
+        [HttpPost]
+        public int GetRoomsNum(String userid)
+        {
+            var data = DbHelperOra.Query("select * from MY_ROOM_APPOINTMENT where USER_ID=" + userid);
+            int num = data.Tables[0].Rows.Count;
+            return num;
         }
     }
 }
